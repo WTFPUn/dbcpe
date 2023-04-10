76ab68420fd64b70135fd44d8d3bcb9a130d06b3
DulyaratMos <97249100+DulyaratMos@users.noreply.github.com>
2023-04-10 14:17:52 +0700

    add api getprofile

---
+++ pages/api/profile/getprofile.jsx
@@ -0,0 +1,58 @@
+import { MongoClient, ServerApiVersion  } from 'mongodb';
+import { jwtdecode } from "@/utils/verify";
+
+export default async function login(req, res) {
+
+    const uri = process.env.MONGO_URI;
+    const client = new MongoClient(uri, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    }
+    );
+
+    const {
+        token, 
+        } = req.body;
+
+        const decoded = jwtdecode(token);
+        const { account_id } = decoded || {};
+
+        if (req.method !== 'POST') {
+            return res.status(405).json({ message: 'Method not allowed', success: false });
+          }
+         
+
+       
+
+
+    try {
+        await client.connect();
+        console.log('Connected to database');
+        const collection = client.db('HotelManage').collection('personal_information');
+    
+        const  profile = await collection.findOne({account_id: account_id});
+        
+
+        return( res.status(200).json({ profile ,message: 'Get profile success', success: true}))
+
+
+
+    }catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: error.message, success: false });
+      } finally {
+        await client.close();
+      }
+
+
+
+
+
+
+
+
+
+}
