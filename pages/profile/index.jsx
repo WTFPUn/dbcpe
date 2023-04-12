d00cca0265522a1458013abb9784dc52fac4723d
WTFPUn <punn_namw@hotmail.com>
2023-04-12 15:34:40 +0700

    update profile update page

--- pages/profile/index.jsx
+++ pages/profile/index.jsx
@@ -47,35 +47,65 @@ export default function profile() {
     }
   }, [token]);
 
+
   const handleEdit = (e) => {
     const { name, value } = e.target;
     setProfile((prevProfile) => ({ ...prevProfile, [name]: value }));
   };
 
+  const onSubmit = () => {
+    if (edit) {
+      fetch("http://localhost:3000/api/profile/updateprofile", {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          "auth-token": token,
+        },
+        body: JSON.stringify({ ...profile, token: token }),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+        });
+    }
+  };
+
+  const onClick = () => {
+    if (edit) {
+      onSubmit();
+    }
+    else {
+      setEdit(!edit);
+    }
+  }
+
   return(
     <Template title={"Edit Profile"}>
       <div className="w-[80%] bg-white self-center rounded-md my-16 place-items-center">
         {profile ? (
-          <form className="flex flex-col w-full items-center">
-            {Object.keys(profile).map((key, index) => {
-              return (
-                <div className="flex flex-col w-10/12 my-4" key={index+key+"frame"}>
-                <label className="text-[#8C8CA1]" key={index+key+"label"}>{key}</label>
-                <input
-                  type="text"
-                  name={key}
-                  id={key}
-                  key={index+key+"input"}
-                  value={profile[key]}
-                  onChange={handleEdit}
-                  className="border border-[#8C8CA1] rounded-md text-base font-light py-1 px-2"
-                  readOnly={!edit}
-                />
-              </div>
-              )
-            }
-          )}
-        </form>
+          <div className="w-full flex flex-col place-items-center">
+            <form className="flex flex-col w-full items-center">
+              {Object.keys(profile).map((key, index) => {
+                return (
+                  <div className="flex flex-col w-10/12 my-4" key={index+key+"frame"}>
+                  <label className="text-[#8C8CA1]" key={index+key+"label"}>{key}</label>
+                  <input
+                    type="text"
+                    name={key}
+                    id={key}
+                    key={index+key+"input"}
+                    value={profile[key]}
+                    onChange={handleEdit}
+                    className="border border-[#8C8CA1] rounded-md text-base font-light py-1 px-2"
+                    readOnly={!edit}
+                  />
+                </div>
+                )
+              }
+            )}
+          </form>
+          <div onClick={() => onClick()} className="bg-[#8C8CA1] text-white rounded-md py-2 px-4 my-4 w-max items-center"> {edit ? "Save" : "Edit"} </div>
+        </div>
         ) : (
           <div className="flex flex-col w-10/12 my-4">
             <div> No Profile Found </div>  
