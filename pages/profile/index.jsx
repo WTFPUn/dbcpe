c924a542d6905ace3832be12a68c615da5dfed9c
WTFPUn <punn_namw@hotmail.com>
2023-04-14 21:59:55 +0700

    update profile component

--- pages/profile/index.jsx
+++ pages/profile/index.jsx
@@ -1,6 +1,8 @@
 import Template from "@/components/Template";
 import React, { useState, useEffect } from 'react';
 import { jwtdecode } from "@/utils/verify";
+// impor link
+import Link from "next/link";
 
 export default function profile() {
   const [token, setToken] = useState("");
@@ -29,7 +31,7 @@ export default function profile() {
   useEffect(() => {
     if (token) {
       const decoded = jwtdecode(token);
-      const { account_id } = decoded;
+      const { account_id } = decoded; 
       fetch(`http://localhost:3000/api/profile/getprofile?${new URLSearchParams({ qArray: Object.keys(profile) })}`
         , {
         method: "GET",
@@ -79,16 +81,80 @@ export default function profile() {
     }
   }
 
+    const link = [
+      {
+        name: "Profile Info",
+        link: "/profile",
+        current: true
+      },
+      {
+        name: "Booking History",
+        link: "/profile/history",
+        current: false
+      }
+    ]
+    const Name = profile.first_name + " " + profile.last_name;
   return(
-    <Template title={"Edit Profile"}>
-      <div className="w-[80%] bg-white self-center rounded-md my-16 place-items-center">
+    <Template title={"Edit Profile"} hscreen>
+      <div className="w-[80%] bg-white self-center rounded-md my-4 place-items-center flex flex-row overflow-y-auto font-oxygen">
+        <div className="w-4/12 flex flex-col bg-[#ECF1F4] h-full place-items-center place-content-center">
+          <p className="mb-8">{Name}</p>
+          {link.map((item, index) => {
+            return (
+              <Link 
+            href={item.link}
+            key={index+"link"}
+            className={`${item.current ? "text-[#4A4A68]" : "text-[#8C8CA1]"}`}
+            >
+              {item.name}
+            </Link>
+            )
+          })}        
+        </div>
         {profile ? (
-          <div className="w-full flex flex-col place-items-center">
-            <form className="flex flex-col w-full items-center">
+          <div className="w-8/12 flex flex-col place-items-center h-full py-4 px-48">
+            <p className="text-3xl font-bold self-start">Edit your profile</p>
+            <form className="flex flex-col h-5/6 w-full items-center overflow-y-auto">
               {Object.keys(profile).map((key, index) => {
+
+                if (key === "date_of_birth"){
+                  return (
+                    <div className="flex flex-col w-full my-4" key={index+key+"frame"}>
+                  <label className="text-black" key={index+key+"label"}>{key}</label>
+                  <input
+                    type="date"
+                    name={key}
+                    id={key}
+                    key={index+key+"input"}
+                    value={profile[key]}
+                    onChange={handleEdit}
+                    className={`border border-[#8C8CA1] rounded-md text-base font-light py-1 px-2 ${!edit ? "bg-[#ECF1F4]" : 'bg-white'}` }
+                    readOnly={!edit}
+                  />
+                </div>
+                  )
+                } 
+                else if (key === 'address') {
+                  return (
+                    <div className="flex flex-col w-full my-4" key={index+key+"frame"}>
+                    <label className="text-black" key={index+key+"label"}>{key}</label>
+                    <textarea
+                      type="text"
+                      name={key}
+                      id={key}
+                      key={index+key+"input"}
+                      value={profile[key]}
+                      onChange={handleEdit}
+                      className={`border border-[#8C8CA1] rounded-md text-base font-light py-1 px-2 ${!edit ? "bg-[#ECF1F4]" : 'bg-white'}` }
+                      readOnly={!edit}
+                    />
+                  </div>
+                  )
+                }
+                else {
                 return (
-                  <div className="flex flex-col w-10/12 my-4" key={index+key+"frame"}>
-                  <label className="text-[#8C8CA1]" key={index+key+"label"}>{key}</label>
+                  <div className="flex flex-col w-full my-4" key={index+key+"frame"}>
+                  <label className="text-black" key={index+key+"label"}>{key}</label>
                   <input
                     type="text"
                     name={key}
@@ -96,15 +162,20 @@ export default function profile() {
                     key={index+key+"input"}
                     value={profile[key]}
                     onChange={handleEdit}
-                    className="border border-[#8C8CA1] rounded-md text-base font-light py-1 px-2"
+                    className={`border border-[#8C8CA1] rounded-md text-base font-light py-1 px-2 ${!edit ? "bg-[#ECF1F4]" : 'bg-white'}` }
                     readOnly={!edit}
                   />
                 </div>
                 )
+                }
               }
             )}
           </form>
-          <div onClick={() => onClick()} className="bg-[#8C8CA1] text-white rounded-md py-2 px-4 my-4 w-max items-center"> {edit ? "Save" : "Edit"} </div>
+          <div className="flex flex-col place-content-start w-full">
+            <p className="text-black">Password</p>
+            <p className="text-[#6C6EF2]">Change my password</p>
+          </div>
+          <div onClick={() => onClick()} className="bg-[#6C6EF2] text-white rounded-md py-2 px-4 my-4 w-full text-center items-center"> {edit ? "Update Info" : "Edit Info"} </div>
         </div>
         ) : (
           <div className="flex flex-col w-10/12 my-4">
