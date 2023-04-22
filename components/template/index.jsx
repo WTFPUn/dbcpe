b0c00f9d3b2f7ceccfe48f12dff2d0ed0369df37
WTFPUn <punn_namw@hotmail.com>
2023-04-22 18:11:52 +0700

    update template layout

--- components/template/index.jsx
+++ components/template/index.jsx
@@ -2,7 +2,7 @@ import Header from "./Header";
 
 export default function Template({children, hscreen, title}) {
   return (
-    <div className={`font-oxygen scroll-smooth w-screen bg-[#0E0E2C] ${hscreen && "h-screen"} flex flex-col`}>
+    <div className={`font-oxygen scroll-smooth w-screen bg-[#0E0E2C] ${hscreen && "h-screen"} flex flex-col place-items-center`}>
       <Header/>
       { title && <h1 className="text-xl text-white font-bold font-dmserif text-center my-5">{title}</h1>}
       {children}
