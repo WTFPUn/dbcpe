aac7b85e31b24aac58a88acf18d15cdb20d84dcc
WTFPUn <punn_namw@hotmail.com>
2023-04-24 22:24:06 +0700

    update success room component

--- components/accommodation/RoomList/index.jsx
+++ components/accommodation/RoomList/index.jsx
@@ -1,8 +1,16 @@
+import Room from "./Room"
+
 export default function RoomList({roomList}) {
-  
+  // console.log("room", roomList)
   return(
-    <div className="w-3/4 bg-white">
-      
+    <div className="w-3/4 bg-white pb-16 rounded-md mt-8 px-16">
+      <div className="w-full text-center text-4xl font-semibold py-16 border-[#8C8CA1] border-b mb-4">Accommodation</div>
+      {roomList.map((room, index) => {
+        return(
+          <Room room={room} key={index}/>
+        )
+      }
+      )}
     </div>
   )
 }
