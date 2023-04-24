d39d6e5d6eb351c95745bbb33a63ceea62831858
WTFPUn <punn_namw@hotmail.com>
2023-04-24 22:23:34 +0700

    add 404 amd 403 page

---
+++ pages/403.jsx
@@ -0,0 +1,8 @@
+export default function Custom403() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-9xl font-bold">403s</h1>
+      <h2 className="text-2xl font-bold">Access denied</h2>
+    </div>
+  )
+}
---
+++ pages/404.jsx
@@ -0,0 +1,8 @@
+export default function custom404() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-9xl font-bold">404s</h1>
+      <h2 className="text-2xl font-bold">Page not found</h2>
+    </div>
+  )
+}
