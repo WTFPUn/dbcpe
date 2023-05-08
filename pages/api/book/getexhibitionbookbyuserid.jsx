f2994057f0196e89ca01cb3e7bc8dc64ec27bf4d
DulyaratMos <97249100+DulyaratMos@users.noreply.github.com>
2023-05-08 19:05:46 +0700

    create api getexhibitionbookbyuserid, getpricebookexhibition

---
+++ pages/api/book/getexhibitionbookbyuserid.jsx
@@ -0,0 +1,152 @@
+import { MongoClient, ServerApiVersion  } from 'mongodb';
+import { jwtdecode } from "@/utils/verify";
+
+export default async function getExhibitionBookById(req, res) {
+
+    const uri = process.env.MONGO_URI;
+    const client = new MongoClient(uri, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    }
+    );
+
+    const token = req.headers["auth-token"];
+    const decoded = jwtdecode(token);
+    const { account_id } = decoded || {};
+
+
+    if (req.method !== 'GET') {
+        return res.status(405).json({ message: 'Method not allowed', success: false });
+      }
+
+
+ try {
+        await client.connect();
+        console.log('Connected to database');
+        const book = client.db('HotelManage').collection('exhibition_booking');
+        const tier = client.db('HotelManage').collection('guess_tier');
+        const role = client.db('HotelManage').collection('role');
+       
+
+        
+        
+        const getbook  =  await book.aggregate( [
+            {
+                $match: {
+                 $and: [
+                    {'account_id': account_id},
+                    {'bookstatus_id': 1}
+
+                 ]
+
+
+                   }
+            },
+
+            
+
+            {
+                $lookup: {
+             from: "exhibition_room",
+             localField: "exhibition_id",
+             foreignField: "exhibition_id",
+             as: "exhibition_room"
+           }
+            },
+            
+        {
+                $lookup: {
+            from: "type_of_exhibition",
+            localField: "exhibition_room.exhibition_type_id",
+            foreignField: "exhibition_type_id",
+            as: "type_of_exhibition"
+            }
+        },{$project:{"_id":0,"exhibition_booking_id":1,"account_id":1,"exhibition_id":1,"book_date":1,"bookstatus_id":1,"checkin_date":1,"checkout_date":1,
+        "exhibition_room.name":1, "type_of_exhibition.type_name":1, "type_of_exhibition.have_microphone":1, "type_of_exhibition.have_projector":1,
+        "type_of_exhibition.have_wifi":1, "type_of_exhibition.have_wifi":1,  "type_of_exhibition.have_whiteboard":1,  "type_of_exhibition.sound_system":1, 
+        "type_of_exhibition.catering_service":1,  "type_of_exhibition.Seating_arrangements":1, "type_of_exhibition.image":1,  }}
+        
+
+          
+        ] ).toArray();
+
+        
+        let count;
+        const  loop = async () => {
+
+        for (let i=0 ; i< getbook.length; i++){
+
+        await  fetch("http://localhost:3000/api/book/getpricebookexhibition", {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                 "auth-token": token,
+                 },
+              
+               body: JSON.stringify({bookid: getbook[i].exhibition_booking_id}),
+            })
+            .then((res) => res.json())
+            .then((data) => {
+              getbook[i]["price_summary"] = data;
+              count = data.book_count
+
+            })
+
+            getbook[i].exhibition_room = getbook[i].exhibition_room[0]
+            getbook[i].type_of_exhibition = getbook[i].type_of_exhibition[0]
+            // console.log("summary = ", getbook[i].price_summary)
+            
+        }
+
+     }
+     
+      await loop();
+
+    
+     //check tier 
+
+     let tierId = -1
+         if(count>= 5 && count< 10){
+            tierId = 0
+         }
+         else if(count>= 10 && count< 15){
+            tierId = 1
+         }
+         else if(count>= 15 && count< 20){
+            tierId = 2
+         }
+         else if(count>= 20){
+            tierId = 3
+         }
+       
+     const getRole = await role.findOne( {"role":0,"sub_role": tierId},{projection:{"_id": 0}});
+     console.log("getRole = ", getRole)
+    
+
+   
+
+        //add user role in message
+
+
+        if (getbook) {
+            return res.status(200).json({book:{getRole,getbook},message: 'get book success', success: true});
+          }
+          else {
+            return res.status(400).json({ message: 'get book failed', success: false });
+          }
+
+
+
+    }catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: error.message, success: false });
+      } finally {
+        await client.close();
+      }
+
+
+
+}
---
+++ pages/api/book/getpricebookexhibition.jsx
@@ -0,0 +1,208 @@
+import { MongoClient, ServerApiVersion  } from 'mongodb';
+import { jwtdecode } from "@/utils/verify";
+
+
+export default async function getpricebookexhibition(req, res) {
+
+    const uri = process.env.MONGO_URI;
+    const client = new MongoClient(uri, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    }
+    );
+
+    const token = req.headers["auth-token"];
+    const decoded = jwtdecode(token);
+    const { account_id } = decoded || {};
+
+
+    const bookid = parseInt(req.body?.bookid);
+
+
+    if (req.method !== 'POST') {
+        return res.status(405).json({ message: 'Method not allowed', success: false });
+      }
+
+
+try {
+        await client.connect();
+        console.log('Connected to database');
+        const bookExhibition = client.db('HotelManage').collection('exhibition_booking');
+        const bookRoom = client.db('HotelManage').collection('room_booking');
+        const tier = client.db('HotelManage').collection('guess_tier');
+        const  room = client.db('HotelManage').collection('exhibition_room')
+        const  typeRooom = client.db('HotelManage').collection('type_of_exhibition')
+       
+
+        let seasonfac = {}
+        //get season setup
+
+        await  fetch("http://localhost:3000/api/bill/getseasonsetup", {
+            method: "GET",
+        })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log("seson eiei : ",data);
+        //   console.log("ans : ",data.seasonFactor.room_price_factor)
+          
+            seasonfac = data.seasonFactor
+          
+        })
+        if(seasonfac){
+            console.log("Hello eiei : ",seasonfac)
+        }
+
+
+        //check tier 
+        let countexhibition
+        const countBookEx = await bookExhibition.aggregate( [
+            {$match:{
+                'account_id': account_id
+            } }
+            ,{ $count: "myCount" }
+         ] ).toArray();
+
+
+
+         if(countBookEx.length === 0){
+            countexhibition = 0
+         }else {
+            countexhibition =   countBookEx[0].myCount
+         }
+         console.log("countEx = ",countexhibition )
+
+
+
+         let countroom
+         const countBookRoom = await bookRoom.aggregate( [
+            {$match:{
+                'account_id': account_id
+            } }
+            ,{ $count: "myCount" }
+         ] ).toArray();
+
+
+
+         if(countBookRoom.length === 0){
+            countroom = 0
+         }else {
+            countroom =   countBookRoom[0].myCount
+         }
+         console.log("countRoom = ",countroom )
+
+
+         let count = countexhibition + countroom
+         console.log("count = ", count)
+        
+         let tierId = -1
+         if(count>= 0 && count< 10){
+            tierId = 0
+         }
+         else if(count>= 10 && count< 15){
+            tierId = 1
+         }
+         else if(count>= 15 && count< 20){
+            tierId = 2
+         }
+         else if(count>= 20){
+            tierId = 3
+         }
+
+         const getTier = await tier.findOne( {"guess_tier_id": tierId},{projection:{"_id": 0,"guess_tier_id": 1,"room_discount_factor": 1,
+         "breakfast_discount_factor": 1,"laundry_discount_factor": 1 }});
+         
+
+         const getBook = await bookExhibition.findOne({"exhibition_booking_id": bookid});
+         const getRoom = await room.findOne({"exhibition_id": getBook.exhibition_id });
+         const getRoomType = await typeRooom.findOne({"exhibition_type_id": getRoom.exhibition_type_id })
+
+         
+         let diffOutIn = (new Date(getBook.checkout_date) - new Date(getBook.checkin_date))/ (1000 * 60 * 60 * 24)
+         let total_price = 0
+         let Totalpricebegin =0
+         let roomfac
+         let servicefac
+
+
+         if(seasonfac){
+
+            roomfac = seasonfac.room_price_factor
+            servicefac = seasonfac.service_factor
+
+            console.log("roomfac",roomfac)
+            console.log("servicefac",servicefac)
+
+            roomfac = roomfac-1
+            servicefac = servicefac -1
+            console.log("roomfac",roomfac)
+
+                if(getTier){
+                    // sumPrice = (breakfast*300*(1 - getTier.breakfast_discount_factor)) + (laundry*50*(1 - getTier.laundry_discount_factor)) + ((1 - getBook.room_discount_factor)*getRoomType.room_price*diffOutIn)
+                    total_price =   (roomfac*(1 - getTier.room_discount_factor)*getRoomType.exhibition_price*diffOutIn) 
+                    
+                    Totalpricebegin =   (roomfac*getRoomType.exhibition_price*diffOutIn) 
+
+                }
+                else{
+                    total_price =  (roomfac*getRoomType.exhibition_price*diffOutIn) 
+                }
+                if(getTier){
+                    return res.status(200).json({ book_count:count,total_price:Totalpricebegin ,discounted_total_price: total_price ,message: 'get price have tier success', success: true});
+                }
+                else if(!getTier){
+                    return res.status(200).json({ book_count:count,total_price,discounted_total_price : 0  ,message: 'get price no tier success', success: true});
+                }
+
+         }
+
+
+         else if(!seasonfac){
+            console.log("wowww")
+
+            if(getTier){
+                // sumPrice = (breakfast*300*(1 - getTier.breakfast_discount_factor)) + (laundry*50*(1 - getTier.laundry_discount_factor)) + ((1 - getBook.room_discount_factor)*getRoomType.room_price*diffOutIn)
+                total_price =  ((1 - getTier.room_discount_factor)*getRoomType.exhibition_price*diffOutIn) 
+                
+                Totalpricebegin =  (getRoomType.exhibition_price*diffOutIn) 
+            }
+            else{
+                total_price =  (getRoomType.exhibition_price*diffOutIn) 
+                
+            }
+
+            if(getTier){
+                return res.status(200).json({ book_count:count,total_price:Totalpricebegin ,discounted_total_price: total_price ,message: 'get price have tier success', success: true});
+            }
+            else if(!getTier){
+                return res.status(200).json({ book_count:count,total_price, discounted_total_price: 0 ,message: 'get price no tier success', success: true});
+            }
+
+         }
+
+
+
+
+        
+
+        else {
+            return res.status(400).json({ message: 'No values price', success: false });
+          }
+
+       
+
+
+
+
+}catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: error.message, success: false });
+  } finally {
+    await client.close();
+  }
+
+
+
+}
