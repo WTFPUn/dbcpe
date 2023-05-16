7b9d5d7984ce8b25de4aa20bb0a2f0d537539248
WTFPUn <punn_namw@hotmail.com>
2023-05-16 20:36:21 +0700

    update confirm bill

--- pages/booking/confirmbook.jsx
+++ pages/booking/confirmbook.jsx
@@ -21,7 +21,30 @@ export default function confirmbook() {
   const [coupon, setCoupon] = useState("");
   const [couponUse, setCouponUse] = useState(false);
   const [couponVerify, setCouponVerify] = useState(false);
-  
+  const [couponObj, setCouponObj] = useState({});
+
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [totalPriceWithCoupon, setTotalPriceWithCoupon] = useState(0);
+
+  const setRoomAfterFetch = async (data) => {
+    setBookRoom(data.book.getbook);
+    let roomTick = [];
+    data.book.getbook?.map((book) => {
+      roomTick.push({ book_id: book.book_id, tick: false, book_type: 0 });
+    });
+    setBookRoomkTick(roomTick);
+    setRoomCount(data.book.getbook.length);
+  }
+
+  const setExhibitionAfterFetch = async (data) => {
+    setBookExhibition(data.book.getbook);
+    let exhibitionTick = [];
+    data.book.getbook?.map((book) => {
+      exhibitionTick.push({ book_id: book.exhibition_booking_id, tick: false, book_type: 1 });
+    });
+    setBookExhibitionTick(exhibitionTick);
+    setExhibitionCount(data.book.getbook.length);
+  }
 
   useEffect(() => {
     const token = localStorage.getItem("token");
@@ -34,60 +57,131 @@ export default function confirmbook() {
         },
       })
       .then((res) => res.json())
-      .then((data) => {
-        setBookRoom(data.book.getbook);
-        let roomTick = [];
-        bookRoom?.map((book) => {
-          roomTick.push({ book_id: book.book_id, tick: false, book_type: 0 });
-        });
-        setBookRoomkTick(roomTick);
-        setRoomCount(data.book.getbook.length);
-      }
+      .then((data) => 
+        setRoomAfterFetch(data)
     );
 
     fetch("/api/book/getexhibitionbookbyuserid", {
-      method: "GET",
+      method: "GET",  
       headers: {
         "Content-Type": "application/json",
         "auth-token": token,
         },
       })
       .then((res) => res.json())
-      .then((data) => {
-        setBookExhibition(data.book.getbook);
-        let exhibitionTick = [];
-        bookExhibition?.map((book) => {
-          exhibitionTick.push({ book_id: book.book_id, tick: false, book_type: 1 });
-        });
-        setBookExhibitionTick(exhibitionTick);
-        setExhibitionCount(data.book.getbook.length);
-      }
+      .then((data) => 
+        setExhibitionAfterFetch(data)
     );
 
-    
+    setTotalPrice(0);
   }, []);
 
-  const handleCouponVerify = async () => {
-    // fetch
+  const handleCouponVerify = async (e) => {
+
+    e.preventDefault();
+    const res = await fetch("/api/bill/getcode?code_name=" + coupon, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await res.json();
+    if (data.success) {
+      setCouponVerify(true);
+      setCouponObj(data.getcode);
+      console.log("total", (data.getcode.code_type ==  "fixed" ? totalPrice - data.getcode.discount_factor : totalPrice  * (1 - data.getcode.discount_factor)));
+      setTotalPriceWithCoupon((data.getcode.code_type ==  "fixed" ? totalPrice - data.getcode.discount_factor : totalPrice  * (1 - data.getcode.discount_factor)));
+      setCouponUse(true);
+    }
+    else {
+      setCouponVerify(false);
+      setCoupon("");
+      setCouponUse(true);
+    };
   }
 
   const handleCancelCoupon = () => {
     setCoupon("");
     setCouponUse(false);
     setCouponVerify(false);
+    setTotalPriceWithCoupon(totalPrice);
   }
 
   const handleRoomTick = (index) => {
+    console.log(index);
+    let index_temp = 0;
+    console.log(bookRoomTick);
     setBookRoomkTick(
       bookRoomTick.map((book, i) => {
+        if (i === index) {
+          console.log(book)
+          book.tick = !book.tick;
+          index_temp = i;
+        }
+        console.log(totalPrice);
+        return book;
+      }
+    ));
+    console.log(bookRoomTick)
+    setTotalPrice(totalPrice + (bookRoomTick[index_temp].tick ? bookRoom[index_temp].price_summary.discounted_total_price : -bookRoom[index_temp].price_summary.discounted_total_price));
+    setTotalPriceWithCoupon(totalPriceWithCoupon + (bookRoomTick[index_temp].tick ? bookRoom[index_temp].price_summary.discounted_total_price : -bookRoom[index_temp].price_summary.discounted_total_price));
+
+  }
+
+  const handleExhibitionTick = (index) => {
+    setBookExhibitionTick(
+      bookExhibitionTick.map((book, i) => {
         if (i === index) {
           book.tick = !book.tick;
         }
         return book;
       }
     ));
+    bookExhibitionTick?.map((book, i) => {
+      if (i === index) {
+        setTotalPrice(totalPrice + (book.tick ? bookExhibition[i].price_summary.discounted_total_price : -bookExhibition[i].price_summary.discounted_total_price));
+        setTotalPriceWithCoupon(totalPriceWithCoupon + (book.tick ? bookExhibition[i].price_summary.discounted_total_price : -bookExhibition[i].price_summary.discounted_total_price));
+      }
+    }
+    );
   }
-  
+
+  const handleConfirmOrder = async () => {
+    const token = localStorage.getItem("token");
+    // get book_list that tick is true
+    let book_list = [];
+    bookRoomTick?.map((book) => {
+      if (book.tick) {
+        book_list.push({ book_id: book.book_id, book_type: 0 });
+      }
+    });
+    bookExhibitionTick?.map((book) => {
+      if (book.tick) {
+        book_list.push({ book_id: book.book_id, book_type: 1 });
+      }
+    });
+
+    const res = await fetch("/api/bill/addbill", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "auth-token": token,
+      },
+      body: JSON.stringify({
+        book_list: book_list,
+        code_id: couponVerify ? couponObj.code_id : null,
+      }),
+    });
+    const data = await res.json();
+    if (data.success) {
+      alert("Order success");
+      window.location.reload();
+    }
+    else {
+      alert("Order fail");
+    }
+  }
+
   return(
     <Template title="Confirmbook" hscreen>
       <div className="bg-white w-3/4 rounded-t-md flex flex-col place-items-center py-2 h-full ">
@@ -135,7 +229,7 @@ export default function confirmbook() {
           <div className="ml-auto w-96 h-[11.25rem]  bg-[#ECF1F4] rounded-md mt-8 py-4 px-8 flex flex-col">
             <div className="w-full flex justify-between">
               <p className="font-semibold">Total price</p>
-              <p className="font-semibold">$220</p>
+              <p className="font-semibold">{totalPriceWithCoupon}</p>
             </div>
             <p className="text-[#8C8CA1] text-sm">2 blablabla</p>
             <div className="mt-4 rounded-md bg-white text-black h-8 relative py-2 place-items-center flex px-2">
@@ -173,7 +267,9 @@ export default function confirmbook() {
             )
               }
             </div>
-            <div className="mt-auto w-full rounded-md bg-[#6C6EF2] text-white text-center py-2 text-sm uppercase">Confirm order NOW</div>
+            <div className="mt-auto w-full rounded-md bg-[#6C6EF2] text-white text-center py-2 text-sm uppercase"
+            onClick={handleConfirmOrder}
+            >Confirm order NOW</div>
           </div>
         </div>
       
