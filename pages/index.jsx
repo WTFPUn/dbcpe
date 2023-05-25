4633103909ecf2340c7bb4ebaafce07f1e862b94
WTFPUn <punn_namw@hotmail.com>
2023-05-25 13:34:06 +0700

    update homepage

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import { useEffect, useState } from 'react'
 import Template from '@/components/Template'
+import Link from 'next/link'
 
 
 
@@ -30,8 +31,19 @@ export default function Home() {
   }
 
   return (
-    <Template>
-      
+    <Template hscreen>
+      <div className='w-full h-[94%] relative'>
+        <img src='/images/homepage/Homepage.png' className='w-full h-full object-fill' />
+        <div className='absolute top-[45%] left-1/4 transform -translate-x-1/2 -translate-y-1/2'>
+          <div className='flex flex-col items-left'>
+            <div className='flex flex-col items-left'>
+              <div className='text-[6rem] font-bold text-white font-dmserif'>Welcome Our</div>
+              <div className='text-[6rem]  font-bold text-white font-dmserif'>MISH Hotel</div>
+              <Link href={"/accommodation"} className=' text-sm text-white font-medium bg-[#6C6EF2] rounded-full px-2 py-2 text-center uppercase w-[20rem] self-center'>explore our accommodation</Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </Template>
   )
 }
