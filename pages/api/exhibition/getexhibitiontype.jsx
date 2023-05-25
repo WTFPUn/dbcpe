234192ef70e7d5be04f612dddb5381af901bd8c7
DulyaratMos <97249100+DulyaratMos@users.noreply.github.com>
2023-05-25 15:13:46 +0700

    delete console.log

--- pages/api/exhibition/getexhibitiontype.jsx
+++ pages/api/exhibition/getexhibitiontype.jsx
@@ -18,7 +18,6 @@ export default async function getExhibitionType(req, res) {
 
     try {
         await client.connect();
-        console.log('Connected to database');
         const collection = client.db('HotelManage').collection('type_of_exhibition');
     
         // const existingAccount = await collection.findOne({email: email});
