03d47bb929251e552c7e06c2729290c6bffd5e71
WTFPUn <punn_namw@hotmail.com>
2023-05-26 12:05:02 +0700

    update library and bill control

--- pages/admin/billcontrol.jsx
+++ pages/admin/billcontrol.jsx
@@ -1,5 +1,7 @@
 import Template from "@/components/Template";
-import { useEffect, useState } from "react";
+import MagnifyIcon from "@/components/icons/MagnifyIcon";
+import Link from "next/link";
+import { use, useEffect, useState } from "react";
 
 export default function billcontrol() {
 
@@ -22,15 +24,99 @@ export default function billcontrol() {
 
   };
 
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      fetch('/api/admin/getallbill',
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            "auth-token": token,
+          },
+        })
+        .then((res) => res.json())
+        .then((data) => {
+          setBill(data.getbill);
+        });
+    }
+  }, []);
 
+  const handleFilterSubmit = () => {
+    const token = localStorage.getItem("token");
+    fetch(`/api/admin/getallbill?bill_id=${filter.bill_id}&paid_status=${filter.paid_status}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "auth-token": token,
+        },
+      })
+      .then((res) => res.json())
+      .then((data) => {
+        setBill(data.getbill);
+      }
+      );
+  }
 
   return(
     <Template hscreen>
       <div className="w-[75%] bg-[#F5F5F5] rounded-t-md h-full mt-24 flex flex-col">
         <div className="w-full h-[20%] text-center text-black text-3xl font-semibold py-16 ">Guest's Bill</div>
-        <div className="flex flex-col w-full h-[80%]">
-          <div className="flex w-full rounded-md">
-
+        <div className="flex flex-col w-full h-[80%] px-32">
+          <div className="flex w-full rounded-md gap-2">
+            <div className="w-4/5 flex bg-[#FAFCFE] rounded-md drop-shadow place-items-center">
+              <div className="ml-4 w-1/6">
+                <MagnifyIcon/>
+              </div>
+              <input
+                  type="text"
+                  className="w-4/6 h-full rounded-md bg-[#FAFCFE] ml-4 appearance-none outline-none px-4 text-[#8C8CA1] py-2 placeholder:text-[#C4C4C4]"
+                  placeholder="Search for guest's bill by bill id"
+                  onChange={(e) => handleFilter(e, "bill_id")}
+                />
+              <select
+                className="w-1/6 border-l border-[#C4C4C4] h-full rounded-r-md bg-[#FAFCFE] ml-4 appearance-none outline-none px-4 text-[#8C8CA1] py-2 placeholder:text-[#C4C4C4]"
+                onChange={(e) => handleFilter(e, "paid_status")}
+              >
+                <option value="">All</option>
+                <option value={1}>Paid</option>
+                <option value={0}>Unpaid</option>
+              </select>
+            </div>
+            <div 
+              className="w-1/5 rounded-md bg-[#6C6EF2] text-white uppercase font-semibold text-center py-2 cursor-pointer"
+              onClick={handleFilterSubmit}
+              > 
+              search
+            </div>
+          </div>
+          <div className="h-[32rem] bg-[#FAFCFE] w-full mt-8 drop-shadow rounded-md p-4 flex flex-col">
+            <div className="text-2xl text-black w-full text-left">Bills</div>
+            <div className="flex flex-col w-full mt-2 h-[10%]">
+              <div className="flex text-black font-semibold w-full">
+                <div className="w-[10%] text-center py-2">No</div>
+                <div className="w-[15%] text-center py-2">Bill ID</div>
+                <div className="w-[15%] text-center py-2">Name</div>
+                <div className="w-[15%] text-center py-2">Billed On</div>
+                <div className="w-[15%] text-center py-2">Status</div>
+                <div className="w-[30%] text-center py-2">Notation</div>
+              </div>
+            </div>
+          <div className="flex flex-col h-[90%] overflow-y-auto">
+            {bill && bill.map((item, index) => (
+                <div className="flex flex-col w-full mt-2" key={index}>
+                  <div className="flex text-[#8C8CA1] w-full">
+                    <div className="w-[10%] text-center py-2">{index + 1}</div>
+                    <div className="w-[15%] text-center py-2">{item.bill_id}</div>
+                    <div className="w-[15%] text-center py-2">{item.full_name}</div>
+                    <div className="w-[15%] text-center py-2">{ new Date(item.create_date).getMonth() + new Date(item.create_date).getDay() + new Date(item.create_date).getFullYear()}</div>
+                    <div className="w-[15%] text-center py-2">{item.paid_status == 1 ? "Paid" : "Unpaid"}</div>
+                    <div className="w-[30%] text-center py-2 text-[#1890FF]"><Link href={"/booking/bill/" + item.bill_id}>Notation</Link></div>
+                  </div>
+                </div>
+              ))}
+          </div> 
           </div>
         </div>
       </div>
--- pages/booking/bill/[bill_id].jsx
+++ pages/booking/bill/[bill_id].jsx
@@ -1,7 +1,10 @@
 import Template from "@/components/Template"
 import Section from "@/components/booking/Section";
+import InfoIcon from "@/components/icons/InfoIcon";
+import { jwtdecode } from "@/utils/verify";
 import { useRouter } from "next/router"
 import { useEffect, useState } from "react"
+
 
 export default function bill() {
   // get bill_id from url
@@ -37,14 +40,61 @@ export default function bill() {
   }, [bill_id]);
 
 
+  const sectionShow = () => {
+    const token = localStorage.getItem("token");
+    const decoded = jwtdecode(token);
+    return decoded.role == 1 ? false : true;
+  }
+
+  const [discountExVisible, setDiscountExVisible] = useState(false);
+  const [paymentmethod, setPaymentmethod] = useState("")
+
+  const paymentMethod = ["Credit/Debit Card", "Paypal", "Bitcoin", "Transfer", "Cash"]
+
+
+  const handleConfirm = (caller) => {
+    const token = localStorage.getItem("token");
+    if (caller == "confirm") {
+      const payload = {
+        bill_id: bill_id,
+        payment_method: paymentmethod,
+    }
+    fetch(`/api/admin/billconfirm`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        'auth-token': token
+        },
+        body: JSON.stringify(payload)
+        });
+  }
+  else if (caller == "cancel") {
+    const payload = {
+      bill_id: bill_id,
+  }
+  fetch(`/api/admin/billcancel`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      'auth-token': token
+      },
+      body: JSON.stringify(payload)
+      });
+  }
+  }
 
   if (bill) {
   return(
     <Template title="Bill">
       <div className="bg-white w-5/6 rounded-t-md flex flex-col place-items-center py-2 h-full ">
-        <Section/>
-        <div className="w-[85%]">
+        {sectionShow() ? 
+        <Section /> :
+        <div className="w-full text-4xl font-semibold text-center h-[15%] pt-4">Guest's bill</div>
+        }
+        <div className="w-[85%] flex flex-col place-items-center mt-8">
+          {sectionShow() &&
           <div className="font-semibold text-xl my-10">Your Bill</div>
+          }
             <div className="flex flex-col rounded-md w-[90%] bg-[#06CBC3] place-content-center">
                 <div className="flex flex-col w-full justify-between px-16 py-8">
                   <div className="flex justify-between">
@@ -77,7 +127,7 @@ export default function bill() {
                   </div>
 
                 </div>
-                <div className="bg-[#ECF1F4] px-16 min-h-[16rem]">
+                <div className="bg-[#ECF1F4] px-16 min-h-[24rem]">
                   <table className=" table-fixed my-8 w-full">
                     <thead>
                       <tr className="text-[#4A4A68] text-lg font-semibold w-full">
@@ -95,15 +145,70 @@ export default function bill() {
                       }
                     </thead>
                   </table>
+                  <div className="text-[#8C8CA1] text-base h-16 w-full flex">
+                      <div className=" w-10/12 text-left font-semibold pl-8">{"Total: "}</div>
+                      <div className=" w-2/12 text-center font-semibold">{bill.price.toFixed(2)}</div>
+                  </div>
+                  <div className="text-[#8C8CA1] text-base h-16 w-full flex">
+                      <div className=" w-10/12 text-left font-semibold flex gap-2">
+                        <div 
+                          className="relative" 
+                          onMouseLeave={() => setDiscountExVisible(false)} 
+                          onMouseEnter={() => setDiscountExVisible(true)}
+                          >
+                          {discountExVisible &&
+                          <div className="absolute bottom-[-2rem] bg-gray-400 text-white text-[.7rem] rounded-md p-2 h-12 w-max ">
+                            <p>Discount price calculated by Tier that calculated by the number of paid book! </p>
+                          </div>
+                          }
+                          <InfoIcon/>
+                          </div>
+                          {"Discounted: "}
+                        </div>
+                      <div className=" w-2/12 text-center font-semibold">{bill.discounted_price.toFixed(2)}</div>
+                  </div>
                 </div>
-                <div className="flex">
-                    <div className="flex flex-col">
-                      <div>
-                        
+                <div className="flex px-8 py-8 justify-between">
+                      <div className="flex flex-col w-1/2 text-left gap-4 font-semibold">
+                        <div className="text-2xl text-[#4A4A68]">{"Date: " + Date(bill.create_date).slice(0, 15)}</div>
+                        <div className="text-2xl text-[#0E0E2C]">{"Due Date: " + bill.pay_due_date}</div>
                       </div>
-                    </div>
+                      <div className="flex flex-col w-1/2 text-right gap-4 font-semibold">
+                        <div className="text-[#4A4A68] text-base font-[550]">Total</div>
+                        <div className="text-[#FAFCFE] text-base font-[550]">{bill.discounted_price.toFixed(2)}</div>
+                      </div>
                 </div>
             </div>
+        </div>
+        <div className="w-[50%] py-4 flex flex-col place-items-center mt-8 gap-4">
+          <select
+            className="w-full bg-[#6C6EF2] text-white text-left px-8 py-4 rounded-md appearance-none"
+            onChange={(e) => setPaymentmethod(e.target.value)}
+          >
+            {paymentMethod.map((type, i) => (
+              <option 
+                className="bg-[#C4C4C4]"
+                key={i} 
+                value={type}>
+                {type}
+              </option>
+            ))}
+          </select>
+          <div className="w-full flex  gap-4">
+            <div 
+              className="border-[#6C6EF2] border text-[#6C6EF2] w-1/2 py-4 uppercase text-center rounded-md font-semibold cursor-pointer"
+              onClick={() => {handleConfirm("cancel")}}
+              >
+              Cancel bill
+            </div>
+            <div
+              className="bg-[#6C6EF2] text-white w-1/2 py-4 uppercase text-center rounded-md font-semibold cursor-pointer"
+              onClick={() => {handleConfirm("confirm")}}
+              >
+              Confirm
+          </div>
+
+          </div>
         </div>
       </div>
     </Template> 
